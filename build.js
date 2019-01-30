3c823a94ab7a28bc6002c1512d238ea6493b2fed
Frankie Simms <fdsimms@gmail.com>
2019-01-30 12:01:23 -0500

    chore: switch to using airtable js wrapper

--- build.js
+++ build.js
@@ -1,61 +1,40 @@
 require("dotenv").config();
 const fs = require("fs");
-const https = require("https");
 const path = require("path");
+const Airtable = require("airtable");
 
 const renderRow = require(`./src/renderRow`).default;
 
 const currentPath = path.basename(__dirname);
 
-const { AIRTABLE_API_KEY, BASE_ID, TABLE_NAME } = process.env;
-const requestUrl = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}?api_key=${AIRTABLE_API_KEY}`;
+const { AIRTABLE_API_KEY, BASE_ID, TABLE_NAME, VIEW } = process.env;
 
 fs.mkdir(`${currentPath}/html`, () => console.log("/html directory created."));
 
-https
-  .get(requestUrl, res => {
-    const { statusCode } = res;
-    const contentType = res.headers["content-type"];
+const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(BASE_ID);
 
-    let error;
-    if (statusCode !== 200) {
-      error = new Error(`Request Failed.\nStatus Code: ${statusCode}`);
-    } else if (!/^application\/json/.test(contentType)) {
-      error = new Error(
-        "Invalid content-type.\n" +
-          `Expected application/json but received ${contentType}`
-      );
-    }
-    if (error) {
-      console.error(error.message);
-      // consume response data to free up memory
-      res.resume();
-      return;
-    }
-
-    res.setEncoding("utf8");
-
-    let rawData = "";
-
-    res.on("data", chunk => {
-      rawData += chunk;
-    });
-
-    res.on("end", () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        parsedData.records.forEach(row => {
-          const slug = row.fields._Slug || row.id;
-          const filepath = `dist/html/${slug}.html`;
-          fs.writeFile(filepath, renderRow(row), () => {
-            console.log(`${filepath} written`);
-          });
-        });
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
+base(TABLE_NAME)
+  .select({
+    maxRecords: 100,
+    view: VIEW
   })
-  .on("error", e => {
-    console.error(`Got error: ${e.message}`);
-  });
+  .eachPage(
+    function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+      records.forEach(row => {
+        const slug = row.fields._Slug || row.id;
+        const filepath = `dist/html/${slug}.html`;
+        fs.writeFile(filepath, renderRow(row), () => {
+          console.log(`${filepath} written`);
+        });
+      });
+
+      // calls page function again while there are still pages left
+      fetchNextPage();
+    },
+    function done(err) {
+      if (err) {
+        console.error(err);
+      }
+    }
+  );
