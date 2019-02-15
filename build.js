662176bd3db810942f8e9f2569cc676f44ba900b
Adam Pash <adam.pash@gmail.com>
2019-02-15 15:16:40 -0800

    fix: check if table has Published field

--- build.js
+++ build.js
@@ -10,6 +10,8 @@ const https = require("https");
 
 const Index = require("./src/components/Index").default;
 const RowPage = require("./src/components/RowPage").default;
+const tableHasPublishedColumn = require("./src/utils/tableHasPublishedColumn")
+  .default;
 
 const renderAsHTMLPage = require(`./src/utils/renderAsHTMLPage`).default;
 
@@ -42,104 +44,110 @@ const alreadyDownloadedAttachments = {};
 
 let currentPage = 0;
 let recordsOnCurrentPage = 0;
-base(TABLE_ID)
-  .select({
-    view: VIEW,
-    filterByFormula: "{Published}"
-  })
-  .eachPage(
-    function page(records, fetchNextPage) {
-      records.forEach(row => {
-        if (!allRows[currentPage]) {
-          allRows.push([]);
-        }
-        const formattedRow = formatAirtableRowData(row);
+tableHasPublishedColumn(base, includePublished =>
+  base(TABLE_ID)
+    .select({
+      view: VIEW,
+      ...(includePublished ? { filterByFormula: "{Published}" } : {})
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(row => {
+          if (!allRows[currentPage]) {
+            allRows.push([]);
+          }
+          const formattedRow = formatAirtableRowData(row);
 
-        const attachmentFields = formattedRow.fields.filter(
-          field =>
-            Array.isArray(field.value) && field.value[0] && field.value[0].size
-        );
+          const attachmentFields = formattedRow.fields.filter(
+            field =>
+              Array.isArray(field.value) &&
+              field.value[0] &&
+              field.value[0].size
+          );
 
-        attachmentFields.forEach(attachmentField => {
-          attachmentField.value.forEach(attachment => {
-            if (!alreadyDownloadedAttachments[attachment.url]) {
-              const newUrl = `/assets/${attachment.id}-${attachment.filename}`;
-              downloadFile(attachment.url, `dist${newUrl}`);
-              alreadyDownloadedAttachments[attachment.url] = true;
-              attachment.url = newUrl;
-            }
+          attachmentFields.forEach(attachmentField => {
+            attachmentField.value.forEach(attachment => {
+              if (!alreadyDownloadedAttachments[attachment.url]) {
+                const newUrl = `/assets/${attachment.id}-${
+                  attachment.filename
+                }`;
+                downloadFile(attachment.url, `dist${newUrl}`);
+                alreadyDownloadedAttachments[attachment.url] = true;
+                attachment.url = newUrl;
+              }
+            });
           });
+
+          const slugField = formattedRow.fields.find(
+            field => field.name === "_Slug"
+          );
+          const slug =
+            (slugField &&
+              !alreadySeenSlugs[slugField.value] &&
+              slugField.value) ||
+            formattedRow.id;
+          alreadySeenSlugs[slug] = true;
+
+          const filepath = `dist/${slug}.html`;
+          allRows[currentPage].push(formattedRow);
+          recordsOnCurrentPage += 1;
+          if (recordsOnCurrentPage >= 10) {
+            recordsOnCurrentPage = 0;
+            currentPage += 1;
+          }
+
+          // write individual resource page files
+          fs.writeFile(
+            filepath,
+            renderAsHTMLPage(<RowPage rowData={formattedRow} />),
+            error => {
+              if (error) {
+                console.error(`Error writing ${filepath}`);
+              } else {
+                console.log(`${filepath} written`);
+              }
+            }
+          );
         });
 
-        const slugField = formattedRow.fields.find(
-          field => field.name === "_Slug"
-        );
-        const slug =
-          (slugField &&
-            !alreadySeenSlugs[slugField.value] &&
-            slugField.value) ||
-          formattedRow.id;
-        alreadySeenSlugs[slug] = true;
-
-        const filepath = `dist/${slug}.html`;
-        allRows[currentPage].push(formattedRow);
-        recordsOnCurrentPage += 1;
-        if (recordsOnCurrentPage >= 10) {
-          recordsOnCurrentPage = 0;
-          currentPage += 1;
+        // calls page function again while there are still pages left
+        fetchNextPage();
+      },
+      err => {
+        if (err) {
+          console.log(err);
         }
 
-        // write individual resource page files
-        fs.writeFile(
-          filepath,
-          renderAsHTMLPage(<RowPage rowData={formattedRow} />),
-          error => {
-            if (error) {
-              console.error(`Error writing ${filepath}`);
-            } else {
-              console.log(`${filepath} written`);
+        const writeFile = (idx, filepath, pagination) =>
+          fs.writeFile(
+            filepath,
+            renderAsHTMLPage(
+              <Index rows={allRows[idx]} pagination={pagination} />
+            ),
+            error => {
+              if (error) {
+                console.error(`Error writing ${filepath}`);
+              }
             }
-          }
-        );
-      });
+          );
 
-      // calls page function again while there are still pages left
-      fetchNextPage();
-    },
-    err => {
-      if (err) {
-        console.log(err);
+        allRows.forEach((row, idx) => {
+          const pageFilepath = `dist/page/${idx + 1}.html`;
+          const indexFilepath = `dist/index.html`;
+          const pagination = {
+            back: idx > 0 ? `/page/${idx}.html` : null,
+            next: idx < allRows.length - 1 ? `/page/${idx + 2}.html` : null
+          };
+          if (idx === 0) {
+            // write index page at /
+            writeFile(idx, indexFilepath, pagination);
+          }
+          // write page files for pagination
+          writeFile(idx, pageFilepath, pagination);
+        });
       }
-
-      const writeFile = (idx, filepath, pagination) =>
-        fs.writeFile(
-          filepath,
-          renderAsHTMLPage(
-            <Index rows={allRows[idx]} pagination={pagination} />
-          ),
-          error => {
-            if (error) {
-              console.error(`Error writing ${filepath}`);
-            }
-          }
-        );
-
-      allRows.forEach((row, idx) => {
-        const pageFilepath = `dist/page/${idx + 1}.html`;
-        const indexFilepath = `dist/index.html`;
-        const pagination = {
-          back: idx > 0 ? `/page/${idx}.html` : null,
-          next: idx < allRows.length - 1 ? `/page/${idx + 2}.html` : null
-        };
-        if (idx === 0) {
-          // write index page at /
-          writeFile(idx, indexFilepath, pagination);
-        }
-        // write page files for pagination
-        writeFile(idx, pageFilepath, pagination);
-      });
-    }
-  );
+    )
+);
 
 fs.copyFile("public/default.css", "dist/main.css", () =>
   fs.readFile("custom/styles.css", "utf-8", (err, data) => {
